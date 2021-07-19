bce27c9931b01d9f199ce750a7d25e90110808f2
renancaraujo <renan.araujo@dashdash.com>
2021-07-19 22:45:28 +0100

    goluan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
+final _overlayText = TextStyle(
+  fontFamily: 'press-start-2p',
+  fontSize: 20,
+  color: BasicPalette.white.color,
+);
+
+final _paintWhite = BasicPalette.white.paint();
+final _paintBorder = BasicPalette.white.paint()..style = PaintingStyle.stroke;
+final _paintRed = BasicPalette.red.paint()..blendMode = BlendMode.lighten;
+final _paintGreen = BasicPalette.green.paint()..blendMode = BlendMode.lighten;
+final _paintBlue = BasicPalette.blue.paint()..blendMode = BlendMode.lighten;
+
 void main() {
-  runApp(GameWidget(game: MyGame()));
+  runApp(GameWidget(game: BreakoutGame()));
 }
 
-class MyGame extends BaseGame {}
+class Bg extends Component with HasGameRef<BreakoutGame> {
+  void render(Canvas c) {
+    c.drawRect(gameRef.size.toRect().deflate(1.0), _paintBorder);
+  }
+
+  @override
+  bool get isHud => true;
+
+  @override
+  int get priority => -1;
+}
+
+class BreakoutGame extends BaseGame with HasDraggableComponents {
+  @override
+  Future<void> onLoad() async {
+    camera.shakeIntensity = 5;
+    viewport = FixedResolutionViewport(Vector2(640, 1280));
+    add(Bg());
+  }
+}
